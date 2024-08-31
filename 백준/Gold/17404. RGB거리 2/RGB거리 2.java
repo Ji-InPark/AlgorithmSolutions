79d451676125a79ba4a0ce3ef9c83f0adc499dde
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2024-08-31 16:04:16 +0900

    [Gold IV] Title: RGB거리 2, Time: 124 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Gold/17404. RGB거리 2/RGB거리 2.java
+++ 백준/Gold/17404. RGB거리 2/RGB거리 2.java
@@ -10,7 +10,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine()), result = 1000000;
         var rgb = new int[n][3];
         var dp = new int[n][3];
 
@@ -21,41 +21,19 @@ public class Main {
             rgb[i][2] = Integer.parseInt(inputs[2]);
         }
 
-        dp[0][0] = rgb[0][0];
-        dp[0][1] = 1000000;
-        dp[0][2] = 1000000;
+        for (int start = 0; start < 3; start++) {
+            dp[0][0] = dp[0][1] = dp[0][2] = 1000000;
+            dp[0][start] = rgb[0][start];
 
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
+            for (int i = 1; i < n; i++) {
+                dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
+                dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
+                dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
+            }
+
+            dp[n - 1][start] = 1000000;
+            result = Math.min(Math.min(result, dp[n - 1][0]), Math.min(dp[n - 1][1], dp[n - 1][2]));
         }
-
-        int result = Math.min(dp[n - 1][1], dp[n - 1][2]);
-
-        dp[0][0] = 1000000;
-        dp[0][1] = rgb[0][1];
-        dp[0][2] = 1000000;
-
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
-        }
-
-        result = Math.min(result, Math.min(dp[n - 1][0], dp[n - 1][2]));
-
-        dp[0][0] = 1000000;
-        dp[0][1] = 1000000;
-        dp[0][2] = rgb[0][2];
-
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
-        }
-
-        result = Math.min(result, Math.min(dp[n - 1][0], dp[n - 1][1]));
 
         bw.write(result + "\n");
         bw.flush();
