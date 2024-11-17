dbf2a3a227eef2e70a4316d6dd0812be74177a4c
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2024-11-17 20:38:34 +0900

    [Gold IV] Title: 전구와 스위치, Time: 232 ms, Memory: 18964 KB -BaekjoonHub

---
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
@@ -0,0 +1,92 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        var startArr = new boolean[n + 2];
+        var endArr = new boolean[n + 2];
+        var originArr = new boolean[n + 2];
+
+        var startInput = br.readLine();
+        var endInput = br.readLine();
+
+        for (int i = 1; i <= n; i++) {
+            originArr[i] = startInput.charAt(i - 1) == '1';
+            endArr[i] = endInput.charAt(i - 1) == '1';
+        }
+
+        int result = Integer.MAX_VALUE, count;
+
+        System.arraycopy(originArr, 0, startArr, 0, n + 2);
+        count = getCount(startArr, endArr, originArr, 0);
+        if (isSame(startArr, endArr)) {
+            result = count;
+        }
+
+        System.arraycopy(originArr, 0, startArr, 0, n + 2);
+        flip(startArr, 1);
+        count = getCount(startArr, endArr, originArr, 1);
+        if (isSame(startArr, endArr)) {
+            result = Math.min(result, count);
+        }
+
+        System.arraycopy(originArr, 0, startArr, 0, n + 2);
+        flip(startArr, n);
+        count = getCount(startArr, endArr, originArr, 1);
+        if (isSame(startArr, endArr)) {
+            result = Math.min(result, count);
+        }
+
+        System.arraycopy(originArr, 0, startArr, 0, n + 2);
+        flip(startArr, 1);
+        flip(startArr, n);
+        count = getCount(startArr, endArr, originArr, 2);
+        if (isSame(startArr, endArr)) {
+            result = Math.min(result, count);
+        }
+
+        if (result == Integer.MAX_VALUE) {
+            result = -1;
+        }
+
+        bw.write(result + "\n");
+        bw.flush();
+    }
+
+    private static int getCount(boolean[] startArr, boolean[] endArr, boolean[] originArr, int count) {
+        for (int i = 2; i < n; i++) {
+            if (startArr[i - 1] != endArr[i - 1]) {
+                flip(startArr, i);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static void flip(boolean[] arr, int index) {
+        for (int i = -1; i < 2; i++) {
+            arr[index + i] = !arr[index + i];
+        }
+    }
+
+    private static boolean isSame(boolean[] startArr, boolean[] endArr) {
+        for (int i = 1; i < startArr.length - 1; i++) {
+            if (startArr[i] != endArr[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
