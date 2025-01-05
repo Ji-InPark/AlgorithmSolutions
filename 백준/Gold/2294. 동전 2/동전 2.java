d2ed163a5be7ded94c9992700692f0e5510d4a3b
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2025-01-05 13:51:54 +0900

    [Gold V] Title: 동전 2, Time: 148 ms, Memory: 16368 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        var numInputs = br.readLine().split(" ");
+        int n = Integer.parseInt(numInputs[0]), k = Integer.parseInt(numInputs[1]);
+
+        var arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+		var dp = new int[150000];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        for (int i = 0; i < n; i++) {
+            dp[arr[i]] = 1;
+            for (int j = 0; j <= k; j++) {
+                if (dp[j] != Integer.MAX_VALUE) {
+                    dp[j + arr[i]] = Math.min(dp[j + arr[i]], dp[j] + 1);
+                }
+            }
+        }
+
+        if (dp[k] == Integer.MAX_VALUE) {
+            bw.write("-1");
+        } else {
+            bw.write(dp[k] + "\n");
+        }
+
+        bw.flush();
+    }
+
+}
