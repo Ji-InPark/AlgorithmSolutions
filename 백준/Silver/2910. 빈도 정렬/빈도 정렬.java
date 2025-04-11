b583c44cdb29575dee97f15d1a5da65cc2bd9bd6
Ji-In Park <81283634+Ji-InPark@users.noreply.github.com>
2025-04-12 02:17:13 +0900

    [Silver III] Title: 빈도 정렬, Time: 124 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,67 +1,51 @@
+
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
 
 public class Main {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        br.readLine();
-        var inputNums = br.readLine().split(" ");
-        var nums = new ArrayList<Integer>();
-        for (var inputNum : inputNums) {
-            nums.add(Integer.parseInt(inputNum));
+        var numInputs = br.readLine().split(" ");
+        int n = Integer.parseInt(numInputs[0]);
+
+        var inputs = br.readLine().split(" ");
+        var map = new LinkedHashMap<Integer, Integer>();
+        for (int i = 0; i < n; i++) {
+            var num = Integer.parseInt(inputs[i]);
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        var map = new HashMap<Integer, Info>();
-
-        for (var num : nums) {
-            if (!map.containsKey(num)) {
-                map.put(num, new Info(num, map.size()));
+        var resultMap = new TreeMap<Integer, ArrayList<Integer>>();
+        for (var entry : map.entrySet()) {
+            if (!resultMap.containsKey(entry.getValue())) {
+                resultMap.put(entry.getValue(), new ArrayList<>());
             }
 
-            var info = map.get(num);
-            info.count++;
+            resultMap.get(entry.getValue()).add(entry.getKey());
         }
 
-        var infos = new ArrayList<>(map.values());
-        Collections.sort(infos);
+        while (!resultMap.isEmpty()) {
+            var now = resultMap.pollLastEntry();
 
-        for (var info : infos) {
-            for (int i = 0; i < info.count; i++) {
-                bw.write(info.num + " ");
+            for (var num : now.getValue()) {
+                for (int i = 0; i < now.getKey(); i++) {
+                    bw.write(num + " ");
+                }
             }
         }
 
-        bw.write("\n");
         bw.flush();
-
     }
-
-    static class Info implements Comparable<Info> {
-
-        int index, count = 0, num;
-
-        public Info(int num, int index) {
-            this.num = num;
-            this.index = index;
-        }
-
-        @Override
-        public int compareTo(Info info) {
-            if (this.count == info.count) {
-                return this.index - info.index;
-            }
-
-            return info.count - this.count;
-        }
-    }
-
 }
+
